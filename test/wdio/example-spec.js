4e824101c264914c6c544a8a9a3b376d1d6a7a5e
Cai,Ben <BCAI@CERNER.COM>
2020-11-18 18:34:02 -0600

    add custom command in before hook.

--- test/wdio/example-spec.js
+++ test/wdio/example-spec.js
@@ -1,5 +1,5 @@
 console.log('*********Outside "describe" block in test*********');
-describe('describe block', () => {
+describeWithFormFactors('describe block', ['small', 'large'], () => {
   console.log('*********Inside "describe" block in test*********');
   it('should log message', () => {
     console.log('*********Inside "it" block in test*********');
